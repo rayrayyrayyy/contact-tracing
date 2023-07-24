c427dd4dfda1365e427090b4a6aeeaf9434c7c59
rayrayyrayyy <allssndr.ray@gmail.com>
2023-07-24 18:37:00 +0800

    pseudocode

---
+++ main.py
@@ -0,0 +1,13 @@
+# Ray Allessandra D. Pacis | BSCPE 1-5
+# create a contact tracing app that can add and search entry
+
+# import modules
+# create GUI window
+# create GUI elements for personal details
+# create GUI elements to ask user if vaccinated or not
+# create GUI elements for symptoms
+# create GUI elements to ask user if he/she had been tested for Covid-19 in the last two weeks
+# create GUI elements for contact person's details
+# add buttons
+
+## collect information
