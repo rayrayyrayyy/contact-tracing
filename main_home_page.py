366425cd6030b150c02f40db363ff21a635add1c
rayrayyrayyy <allssndr.ray@gmail.com>
2023-07-26 11:33:31 +0800

    create 2 more files; home page and seach data

---
+++ main_home_page.py
@@ -0,0 +1,8 @@
+# main program/this file is the one to be executed
+# import modules
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import *
+import customtkinter as ctk
+
---
+++ search_data.py
@@ -0,0 +1,8 @@
+# import modules
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import *
+import customtkinter as ctk
+from data_entry import DataEntry
+
