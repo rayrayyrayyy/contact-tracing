084429a88d2a52f36930aa6d37e722dd7d6e7d6f
rayrayyrayyy <allssndr.ray@gmail.com>
2023-07-26 11:36:19 +0800

    create window for home page

--- main_home_page.py
+++ main_home_page.py
@@ -5,4 +5,16 @@ from tkinter import ttk
 from tkinter import messagebox
 from tkinter import *
 import customtkinter as ctk
+from data_entry import DataEntry
 
+# create window
+home_page = ctk.CTk()
+home_page.title("COVID-19 Contact Tracing APP")
+home_page.geometry("1100x950")
+home_page.config( bg = "#FFF7D4")
+
+
+
+
+
+home_page.mainloop()
