3f996062045b8c9097ce7836cf4a819eb25f17d8
rayrayyrayyy <allssndr.ray@gmail.com>
2023-07-27 06:48:57 +0800

    fix checkboxes and variables; edit submit button

--- data_entry.py
+++ data_entry.py
@@ -7,6 +7,7 @@ from tkinter import ttk
 from tkinter import messagebox
 from tkinter import *
 import customtkinter as ctk
+import csv
 
 class DataEntry(): # put in class
 
@@ -72,21 +73,21 @@ class DataEntry(): # put in class
         self.vaccinated_label = ctk.CTkLabel(self.vaccinated_frame, text=" Are you vaccinated for COVID-19?")
         self.vaccinated_label.grid(row=0, column=0)
 
-        self.radiobutton_var = ctk.IntVar()
+        self.radiobutton_var = ctk.StringVar(value=0)
 
-        self.radio_button_1 = ctk.CTkRadioButton(self.vaccinated_frame, variable=self.radiobutton_var, text='Not yet', value=1, hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.radio_button_1 = ctk.CTkRadioButton(self.vaccinated_frame, variable=self.radiobutton_var, text='Not yet', value='Not vaccinated', hover_color='#FFD95A', fg_color='#4C3D3D')
         self.radio_button_1.grid(row=3, column=0)
 
-        self.radio_button_2 = ctk.CTkRadioButton(self.vaccinated_frame, variable=self.radiobutton_var, text='1st Dose', value=2, hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.radio_button_2 = ctk.CTkRadioButton(self.vaccinated_frame, variable=self.radiobutton_var, text='1st Dose', value='1st Dose', hover_color='#FFD95A', fg_color='#4C3D3D')
         self.radio_button_2.grid(row=4, column=0)
 
-        self.radio_button_3 = ctk.CTkRadioButton(self.vaccinated_frame, variable=self.radiobutton_var, text='2nd Dose', value=3, hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.radio_button_3 = ctk.CTkRadioButton(self.vaccinated_frame, variable=self.radiobutton_var, text='2nd Dose', value='2nd Dose', hover_color='#FFD95A', fg_color='#4C3D3D')
         self.radio_button_3.grid(row=5, column=0)
 
-        self.radio_button_4 = ctk.CTkRadioButton(self.vaccinated_frame, variable=self.radiobutton_var, text='1st Booster Shot', value=4, hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.radio_button_4 = ctk.CTkRadioButton(self.vaccinated_frame, variable=self.radiobutton_var, text='1st Booster Shot', value='1st booster shot', hover_color='#FFD95A', fg_color='#4C3D3D')
         self.radio_button_4.grid(row=3, column=2)
 
-        self.radio_button_5 = ctk.CTkRadioButton(self.vaccinated_frame, variable=self.radiobutton_var, text='2nd Booster Shot', value=5, hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.radio_button_5 = ctk.CTkRadioButton(self.vaccinated_frame, variable=self.radiobutton_var, text='2nd Booster Shot', value='Complete booster shot', hover_color='#FFD95A', fg_color='#4C3D3D')
         self.radio_button_5.grid(row=4, column=2)
 
         # create GUI elements for symptoms
@@ -96,31 +97,41 @@ class DataEntry(): # put in class
         self.symptoms_label = ctk.CTkLabel(self.symptoms_frame, text=" *Please select all symptoms that you have experienced for the past 7 days.")
         self.symptoms_label.grid(row=0, column=0)
 
-        self.checkbox_1 = ctk.CTkCheckBox(self.symptoms_frame, text='Sore throat', hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.sore_throat_symptom = BooleanVar()
+        self.checkbox_1 = ctk.CTkCheckBox(self.symptoms_frame, text='Sore throat', hover_color='#FFD95A', fg_color='#4C3D3D', variable=self.sore_throat_symptom, onvalue = True, offvalue = False)
         self.checkbox_1.grid(row=3)
 
-        self.checkbox_2 = ctk.CTkCheckBox(self.symptoms_frame, text='Cough', hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.cough_symptom = BooleanVar()
+        self.checkbox_2 = ctk.CTkCheckBox(self.symptoms_frame, text='Cough', hover_color='#FFD95A', fg_color='#4C3D3D', variable=self.cough_symptom, onvalue = True, offvalue = False)
         self.checkbox_2.grid(row=4)
 
-        self.checkbox_3 = ctk.CTkCheckBox(self.symptoms_frame, text='Fever', hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.fever_symptom = BooleanVar()
+        self.checkbox_3 = ctk.CTkCheckBox(self.symptoms_frame, text='Fever', hover_color='#FFD95A', fg_color='#4C3D3D', variable=self.fever_symptom, onvalue = True, offvalue = False)
         self.checkbox_3.grid(row=5)
 
-        self.checkbox_4 = ctk.CTkCheckBox(self.symptoms_frame, text='Loss of taste', hover_color='#FFD95A', fg_color='#4C3D3D')
+
+        self.loss_taste_symptom = BooleanVar()
+        self.checkbox_4 = ctk.CTkCheckBox(self.symptoms_frame, text='Loss of taste', hover_color='#FFD95A', fg_color='#4C3D3D', variable=self.loss_taste_symptom, onvalue = True, offvalue = False)
         self.checkbox_4.grid(row=4, column=3)
 
-        self.checkbox_5 = ctk.CTkCheckBox(self.symptoms_frame, text='Loss of smell', hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.loss_smell_symptom = BooleanVar()
+        self.checkbox_5 = ctk.CTkCheckBox(self.symptoms_frame, text='Loss of smell', hover_color='#FFD95A', fg_color='#4C3D3D', variable=self.loss_smell_symptom, onvalue = True, offvalue = False)
         self.checkbox_5.grid(row=5, column=3)
 
-        self.checkbox_6 = ctk.CTkCheckBox(self.symptoms_frame, text='Shortness of breath', hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.breathlessness_symptom = BooleanVar()
+        self.checkbox_6 = ctk.CTkCheckBox(self.symptoms_frame, text='Shortness of breath', hover_color='#FFD95A', fg_color='#4C3D3D', variable=self.breathlessness_symptom, onvalue = True, offvalue = False)
         self.checkbox_6.grid(row=3, column=3)
 
-        self.checkbox_7 = ctk.CTkCheckBox(self.symptoms_frame, text='Runny nose', hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.runny_nose_symptom = BooleanVar()
+        self.checkbox_7 = ctk.CTkCheckBox(self.symptoms_frame, text='Runny nose', hover_color='#FFD95A', fg_color='#4C3D3D', variable=self.runny_nose_symptom, onvalue = True, offvalue = False)
         self.checkbox_7.grid(row=6)
 
-        self.checkbox_8 = ctk.CTkCheckBox(self.symptoms_frame, text='Fatigue', hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.fatigue_symptom = BooleanVar()
+        self.checkbox_8 = ctk.CTkCheckBox(self.symptoms_frame, text='Fatigue', hover_color='#FFD95A', fg_color='#4C3D3D', variable=self.fatigue_symptom, onvalue = True, offvalue = False)
         self.checkbox_8.grid(row=7)
 
-        self.checkbox_9 = ctk.CTkCheckBox(self.symptoms_frame, text='None of the above', hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.no_symptom = BooleanVar()
+        self.checkbox_9 = ctk.CTkCheckBox(self.symptoms_frame, text='None of the above', hover_color='#FFD95A', fg_color='#4C3D3D', variable=self.no_symptom, onvalue = True, offvalue = False)
         self.checkbox_9.grid(row=6, column=3)
 
         # create GUI elements to ask user if he/she had been tested for Covid-19 in the last two weeks
@@ -130,18 +141,18 @@ class DataEntry(): # put in class
         self.tested_label = ctk.CTkLabel(self.tested_frame, text=" Have you been tested for COVID-19 in the last 14 days?")
         self.tested_label.grid(row=0, column=0)
 
-        self.radiobutton_tested_var = ctk.IntVar()
+        self.radiobutton_tested_var = ctk.StringVar(value=0)
 
-        self.radio_button_6 = ctk.CTkRadioButton(self.tested_frame, variable=self.radiobutton_tested_var, text='NO', value=1, hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.radio_button_6 = ctk.CTkRadioButton(self.tested_frame, variable=self.radiobutton_tested_var, text='NO', value='No', hover_color='#FFD95A', fg_color='#4C3D3D')
         self.radio_button_6.grid(row=3, column=0)
 
-        self.radio_button_7 = ctk.CTkRadioButton(self.tested_frame, variable=self.radiobutton_tested_var, text='YES - positive', value=2, hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.radio_button_7 = ctk.CTkRadioButton(self.tested_frame, variable=self.radiobutton_tested_var, text='YES - positive', value='Yes(positive)', hover_color='#FFD95A', fg_color='#4C3D3D')
         self.radio_button_7.grid(row=4, column=0)
 
-        self.radio_button_8 = ctk.CTkRadioButton(self.tested_frame, variable=self.radiobutton_tested_var, text='YES - negative', value=3, hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.radio_button_8 = ctk.CTkRadioButton(self.tested_frame, variable=self.radiobutton_tested_var, text='YES - negative', value='Yes(negative)', hover_color='#FFD95A', fg_color='#4C3D3D')
         self.radio_button_8.grid(row=5, column=0)
 
-        self.radio_button_9 = ctk.CTkRadioButton(self.tested_frame, variable=self.radiobutton_tested_var, text='YES - pending', value=4, hover_color='#FFD95A', fg_color='#4C3D3D')
+        self.radio_button_9 = ctk.CTkRadioButton(self.tested_frame, variable=self.radiobutton_tested_var, text='YES - pending', value='Yes(pending)', hover_color='#FFD95A', fg_color='#4C3D3D')
         self.radio_button_9.grid(row=6, column=0)
 
         # create GUI elements for contact person's details
@@ -211,8 +222,49 @@ class DataEntry(): # put in class
         exit_button = ctk.CTkButton(self.root_window, text= "EXIT", width=90, height=50, corner_radius=10, hover_color='#8B6508', fg_color='#4C3D3D', command = exit)
         exit_button.place(x=260, y=840)
 
+
+        def submit_data(): # define function to save data
+            user_name = self.last_name_entry.get(), self.first_name_entry.get(), self.middle_name_entry.get()
+            age = self.age_entry.get()
+            email = self.email_entry.get()
+            number = self.number_entry.get()
+            address = self.address_entry.get()
+            vaccinated = self.radiobutton_var.get()
+            tested = self.radiobutton_tested_var.get()
+            contact_person = self.name_entry.get()
+            contact_number = self.contact_number_entry.get()
+            relationship = self.relationship_entry.get()
+            contact_email = self.email_contact_entry.get()
+
+            symptoms = {"Sore throath": self.sore_throat_symptom.get(),
+            "Cough": self.cough_symptom.get(),
+            "Fever": self.fever_symptom.get(),
+            "Loss of taste": self.loss_taste_symptom.get(),
+            "Loss of smell": self.loss_smell_symptom.get(),
+            "Shortness of breath": self.breathlessness_symptom.get(),
+            "Fatigue": self.fatigue_symptom.get(),
+            "None of the above": self.no_symptom.get()}
+
+            try:
+                if not (user_name and age and email and number and address and vaccinated and tested and contact_person and contact_number and relationship and contact_email):
+                    messagebox.showerror('NOTICE', 'Please enter all fields.')
+                if symptoms == '':
+                    messagebox.showerror('ERROR', 'Check atleast one of the following symptoms or None of the above.')
+                    return
+                else:
+                    user_age = int(age)
+                    cp_number = int(number)
+                    contact_num = int(contact_number)
+                    with open('data_entries.cvs', 'a', newline='') as file:
+                        data_input = csv.writer(file)
+                        data_input.writerow([user_name, age, email, number, address, vaccinated, tested, contact_person, contact_number, relationship, contact_email])
+                    messagebox.showinfo('SUCCESS', 'Data saved.')
+            except ValueError:
+                messagebox.showerror('ERROR', 'age and contact numbers should be an integer')              
+            except:
+                messagebox.showerror('ERROR', 'Error Occured.')
         # add submit button
-        submit_button = ctk.CTkButton(self.root_window, text = "ENTER", width=90, height=50, corner_radius=10, hover_color='#8B6508', fg_color='#4C3D3D')
+        submit_button = ctk.CTkButton(self.root_window, text = "ENTER", width=90, height=50, corner_radius=10, hover_color='#8B6508', fg_color='#4C3D3D', command=submit_data)
         submit_button.place(x=795, y=840)
 
     def execute(self):
